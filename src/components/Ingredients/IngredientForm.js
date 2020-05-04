5f42dd2f02a47fde3f71dc42dd150fc175bc6b3e
Joel Brown <surfer_43@hotmail.com>
2020-05-04 11:22:31 -0400

    adding in array destructuring for clarity

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,11 +4,7 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const inputState = useState({title: '', amount: ''});
-
-  // const [title, setTitle] = useState('');
-  // const [amount, setAmount] = useState('');
-
+  const [ inputState, setInputState ] = useState({title: '', amount: ''});
   
   const submitHandler = event => {
     event.preventDefault();
@@ -21,7 +17,7 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState[0].title} onChange={ event => {
+            <input type="text" id="title" value={inputState.title} onChange={ event => {
               const titleValue = event.target.value;
               /**
                * this is the closure way of doing updates.
@@ -29,13 +25,12 @@ const IngredientForm = React.memo(props => {
                * the event.target.value needs to be new every time the onChange funciton is called.
                * and passed into the inputState[1] update function
                */
-              inputState[1]( prevState => ({ title: titleValue, amount: prevState.value }))
+              setInputState( prevState => ({ title: titleValue, amount: prevState.value }))
             }}/>
-            {/* <input type="text" id="title" value={title} onChange={event => setTitle(event.target.value)}/> */}
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState[0].amount}  onChange={ event => {
+            <input type="number" id="amount" value={inputState.amount}  onChange={ event => {
               /**
                * this is the closure way of doing updates.
                * the event.target.value needs to be new every time the onChange funciton is called.
@@ -43,9 +38,8 @@ const IngredientForm = React.memo(props => {
                * and passed into the inputState[1] update function
                */
               const amountValue = event.target.value;
-              inputState[1]( prevState => ({ title: prevState.title, amount: amountValue }) )
+              setInputState( prevState => ({ title: prevState.title, amount: amountValue }) )
             }}/>
-            {/* <input type="number" id="amount" value={amount}  onChange={event => setAmount(event.target.value)}/> */}
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
