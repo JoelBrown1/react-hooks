69c8cf30f9222accd5384f0199ae9c72fcdaa383
Joel Brown <surfer_43@hotmail.com>
2020-05-04 13:19:09 -0400

    adding ingredients and storing them in firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [ userIngredients, setUserIngredients ] = useState([]);
+
+  const addIngredientHandler = (ing) => {
+    fetch('https://react-hooks-9a19a.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ing),
+      headers: {'Content-Type': 'application:json'}
+    }).then(resp => {
+      console.log("what is in the resp: ", resp);
+      return resp.json();
+    }).then(responseData => {
+      console.log('what is in the responseData of fetch response: ', responseData);
+      setUserIngredients( prevState  => [ ...prevState, {id : responseData.name, ...ing} ] );
+    });
+  }
+
+  const removeIngredient = (ingId) => {
+    setUserIngredients( prevState => prevState.filter( ing => ing.id !== ingId));
+  }
+
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredient}/>
       </section>
     </div>
   );
