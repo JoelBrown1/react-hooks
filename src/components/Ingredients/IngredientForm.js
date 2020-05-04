831d3b7c013f1755170139a85b391f8c7c3fe57c
Joel Brown <surfer_43@hotmail.com>
2020-05-04 11:35:13 -0400

    using multiple useState hooks

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,8 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const [ inputState, setInputState ] = useState({title: '', amount: ''});
+  const [ title, setTitle ] = useState('');
+  const [ amount, setAmount ] = useState('');
   
   const submitHandler = event => {
     event.preventDefault();
@@ -17,28 +18,21 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState.title} onChange={ event => {
-              const titleValue = event.target.value;
+            <input type="text" id="title" value={title} onChange={ event => {
               /**
-               * this is the closure way of doing updates.
-               * event.target.value is only good for a single iteration of the onChange function
-               * the event.target.value needs to be new every time the onChange funciton is called.
-               * and passed into the inputState[1] update function
+               * with multiple uses of setState, we don't need to capture 
+               * the previous state and update multiple values
+               * We don't need to use full closure functions either as 
+               * event.target.value is always refreshed for us and is available to update
+               * the state with
                */
-              setInputState( prevState => ({ title: titleValue, amount: prevState.value }))
+              setTitle( event.target.value );
             }}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState.amount}  onChange={ event => {
-              /**
-               * this is the closure way of doing updates.
-               * the event.target.value needs to be new every time the onChange funciton is called.
-               * event.target.value is only good for a single iteration of the onChange function
-               * and passed into the inputState[1] update function
-               */
-              const amountValue = event.target.value;
-              setInputState( prevState => ({ title: prevState.title, amount: amountValue }) )
+            <input type="number" id="amount" value={amount}  onChange={ event => {
+              setAmount( event.target.value )
             }}/>
           </div>
           <div className="ingredient-form__actions">
